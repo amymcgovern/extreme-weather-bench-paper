a5bb8c4966cbe4d78713bf3d544cf70bd0f8a760
amymcgovern <dramymcgovern@gmail.com>
2026-01-02 17:06:51 +0000

    removing test code for array lake

--- src/data/test_hans.py
+++
@@ -1,9 +0,0 @@
-from arraylake import Client
-import xarray as xr
-client = Client()
-repo_name = "brightband/ecmwf"
-group_name = "forecast-archive/ewb-hres"
-branch_name = "main"
-repo = client.get_repo(repo_name)
-session = repo.readonly_session(branch_name)
-ds = xr.open_zarr(session.store, group=group_name)
